d0591e1d6e0dcf22918b50d09790eb2e864fd448
Paul Herron <pol.herron@gmail.com>
2020-01-16 15:48:02 +0000

    Add project files.

---
+++ ADOETBLearning/DataConnectionForm.Designer.cs
@@ -0,0 +1,74 @@
+﻿namespace ADOETBLearning
+{
+    partial class DataConnectionForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dgvStudent = new System.Windows.Forms.DataGridView();
+            this.txtFName = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvStudent)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dgvStudent
+            // 
+            this.dgvStudent.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvStudent.Location = new System.Drawing.Point(99, 110);
+            this.dgvStudent.Name = "dgvStudent";
+            this.dgvStudent.Size = new System.Drawing.Size(672, 150);
+            this.dgvStudent.TabIndex = 0;
+            // 
+            // txtFName
+            // 
+            this.txtFName.Location = new System.Drawing.Point(99, 338);
+            this.txtFName.Name = "txtFName";
+            this.txtFName.Size = new System.Drawing.Size(100, 20);
+            this.txtFName.TabIndex = 1;
+            this.txtFName.TextChanged += new System.EventHandler(this.txtFName_TextChanged);
+            // 
+            // DataConnectionForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.txtFName);
+            this.Controls.Add(this.dgvStudent);
+            this.Name = "DataConnectionForm";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.DataConnectionForm_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dgvStudent)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dgvStudent;
+        private System.Windows.Forms.TextBox txtFName;
+    }
+}
+
---
+++ ADOETBLearning/DataConnectionForm.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ADOETBLearning
+{
+    public partial class DataConnectionForm : Form
+    {
+        public DataConnectionForm()
+        {
+            InitializeComponent();
+        }
+
+        private void DataConnectionForm_Load(object sender, EventArgs e)
+        {
+            String str = ConfigurationManager.ConnectionStrings["sqlConnection"].ToString();
+
+            SqlConnection conn = new SqlConnection(str);
+
+            //Create the command
+            SqlCommand cmd = new SqlCommand("SELECT * FROM students", conn);
+
+            //Open the connection
+            conn.Open();
+
+            //Add the reader
+            SqlDataReader reader = cmd.ExecuteReader();
+
+            //Create a data tableto hld the data
+            DataTable dt = new DataTable();
+
+            //Load the data into the table
+            dt.Load(reader);
+
+            //Connect the DataGridView to the table
+            dgvStudent.DataSource = dt;
+
+            //Refresh the grid view control so that it dieplays the data.
+            dgvStudent.Refresh();
+
+            //Close the conection
+            conn.Close();
+        }
+
+        private void txtFName_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
